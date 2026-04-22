63264989f4ba9b74a163e234f0fec59ab431273c
MelanyAA10 <melanyarrietatec@gmail.com>
2026-04-22 11:09:14 -0600

    x

--- target/debug/deps/servidor-788b6b190da3e3f4.d
+++ target/debug/deps/servidor-788b6b190da3e3f4.d
@@ -1,6 +1,6 @@
-C:\Users\melan\OneDrive - Estudiantes ITCR\Lenguajes\SpotiCry\target\debug\deps\servidor-788b6b190da3e3f4.d: src\main.rs src\models\mod.rs src\models\song.rs src\models\playlist.rs src\protocol\mod.rs src\protocol\messages.rs src\server\mod.rs src\server\handler.rs
+C:\Users\melan\OneDrive - Estudiantes ITCR\Git Hub\SpotiCry\target\debug\deps\servidor-788b6b190da3e3f4.d: src\main.rs src\models\mod.rs src\models\song.rs src\models\playlist.rs src\protocol\mod.rs src\protocol\messages.rs src\server\mod.rs src\server\handler.rs
 
-C:\Users\melan\OneDrive - Estudiantes ITCR\Lenguajes\SpotiCry\target\debug\deps\libservidor-788b6b190da3e3f4.rmeta: src\main.rs src\models\mod.rs src\models\song.rs src\models\playlist.rs src\protocol\mod.rs src\protocol\messages.rs src\server\mod.rs src\server\handler.rs
+C:\Users\melan\OneDrive - Estudiantes ITCR\Git Hub\SpotiCry\target\debug\deps\libservidor-788b6b190da3e3f4.rmeta: src\main.rs src\models\mod.rs src\models\song.rs src\models\playlist.rs src\protocol\mod.rs src\protocol\messages.rs src\server\mod.rs src\server\handler.rs
 
 src\main.rs:
 src\models\mod.rs:
--- target/debug/deps/zmij-ff89e32e9f2c4004.d
+++ target/debug/deps/zmij-ff89e32e9f2c4004.d
@@ -1,6 +1,6 @@
-C:\Users\melan\Downloads\spotiCry_clean\spotiCry\target\debug\deps\zmij-ff89e32e9f2c4004.d: C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
+C:\Users\melan\OneDrive - Estudiantes ITCR\Git Hub\SpotiCry\target\debug\deps\zmij-ff89e32e9f2c4004.d: C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
 
-C:\Users\melan\Downloads\spotiCry_clean\spotiCry\target\debug\deps\libzmij-ff89e32e9f2c4004.rmeta: C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
+C:\Users\melan\OneDrive - Estudiantes ITCR\Git Hub\SpotiCry\target\debug\deps\libzmij-ff89e32e9f2c4004.rmeta: C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
 
 C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs:
 C:\Users\melan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs:
